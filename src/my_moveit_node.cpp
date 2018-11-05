c911de905d5247ddc53d484e8e4db578e006ce62
harumo11 <s_harumo@hotmail.co.jp>
2018-11-05 16:08:19 +0900

    [update]

--- src/my_moveit_node.cpp
+++ src/my_moveit_node.cpp
@@ -23,13 +23,39 @@ int main(int argc, char* argv[])
 	//panda_arm : Planning Group (rviz/MotionPlaning/Planning Request/Planning Group)
 	static const std::string PLANNING_GROUP = "panda_arm";
 
+	//move_groupクラスはプランニンググループの名前だけを用いてセットアップ出来ます
 	moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
+	//planning_sceneクラスを用いて接触可能な物体をバーチャル空間に出現させます
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
+	//パフォーマンス改善のためにplanning groupはポインタを参照するときがあります
 	const robot_state::JointModelGroup* joint_model_group = 
 		move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
+	//表示
+	
+	//
+	//基本的な情報の取得
+	//
+	
+	//ロボットに対する参照フレームの名前を表示
+	ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group.getPlanningFrame().c_str());
+
+	//ロボットのエンドエフェクタの名前を表示
+	ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
+
+	//ロボットの全グループのリストを表示
+	ROS_INFO_NAMED("tutorial", "Available Planning Groups: ");
+	//std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
+
+	
+	//
+	//ゴールへのプランニング
+	//
+	
+	//現在の位置の表示
+	std::cout << move_group.getCurrentPose() << std::endl;
 	
 	return 0;
 }
